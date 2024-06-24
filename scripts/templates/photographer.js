96032e16a0436ba681b44dfb0e26b19c9f72290b
Paul Glatt <paul.glatt57@gmail.com>
2024-06-24 04:14:59 +0200

    affichage éléments homepage

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,25 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const location = document.createElement( 'p' );
+        const pTagline = document.createElement( 'p' );
+        const pPrice = document.createElement( 'p' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        location.textContent = `${city}, ${country}`;
+        pTagline.textContent = tagline;
+        pPrice.textContent = `${price}€/jour`;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(location)
+        article.appendChild(pTagline)
+        article.appendChild(pPrice)
         return (article);
     }
     return { name, picture, getUserCardDOM }
